b26f72994ac53e9064e8cc7c267632ef49a3caf7
ALINE DE SOUZA <aline_souza7@estudante.sesisenai.org.br>
2022-12-07 20:32:05 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,25 @@ export class AppComponent implements OnInit {
 
   // variáveis para conseguir conectar à API
   tags: object;
-  selectedTag: string
+  selectedTag: string;
   linkImage: string;
+  linkImageTerceiro: string;
   statusCode: string;
+  numero: string;
+  gatos: string[] = []
 
-  constructor (private appService: AppService) {
+  constructor(private appService: AppService) {
   }
 
   ngOnInit(): void {
     this.appService.getTags()
       .subscribe(tags => {
         this.tags = tags.message
-        console.log(this.tags)
+      })
+
+    this.appService.getGatos()
+      .subscribe(gatos => {
+        this.gatos = gatos
       })
   }
 
@@ -40,5 +47,16 @@ export class AppComponent implements OnInit {
   consultar(valor) {
     console.log(this.selectedTag);
     this.linkImage = 'https:\/\/randomfox.ca\/images\/' + valor + '.jpg';
+  }
+
+  consultarTerceiro() {
+    console.log(this.selectedTag);
+    this.linkImageTerceiro = 'https://http.cat/' + Number(this.numero)
+  }
+
+  terceiroModal: boolean = false;
+
+  abrirTerceiroModal() {
+    this.terceiroModal = !this.terceiroModal;
   }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -11,5 +11,11 @@ export class AppService {
     getTags(): Observable<{message: object}> {
         return this.httpClient.get<{message: object}>('https://randomfox.ca/floof/')
     }
+
+    getGatos(): Observable<string[]> {
+        return this.httpClient.get<string[]>('https://cataas.com/api/tags')
+    }
+
+    
 }
 
